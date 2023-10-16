d3a08529df4582e5386a5bf7bc3e60761e79415f
nccharles <nccharles1@gmail.com>
2023-10-16 14:18:50 +0200

    adding a schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,34 +1,34 @@
-const gql = require('graphql-tag');
-
+const gql = require("graphql-tag");
+ 
 const typeDefs = gql`
   type Query {
-    "Query to get tracks array for the homepage grid"
+    "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
   }
-
+   
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
     "The track's title"
     title: String!
-    "The track's main Author"
+    "The track's main author"
     author: Author!
-    "The track's illustration to display in track card or track page detail"
+    "The track's main illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
   }
-
-  "Author of a complete Track or a Module"
+   
+  "Author of a complete Track"
   type Author {
     id: ID!
     "Author's first and last name"
     name: String!
-    "Author's profile picture"
+    "Author's profile picture url"
     photo: String
   }
 `;
-
+ 
 module.exports = typeDefs;
